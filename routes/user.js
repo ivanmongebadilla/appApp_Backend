b9d8c6aab91d981cb06ea4931f6b00e434ea28dd
Ivan Monge <ivanmongebadilla@gmail.com>
2026-02-24 15:27:44 -0800

    Add user to DB

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 import { Router, json } from "express";
 import { userValidation } from "../middlewares/schemavalidations.js";
+import { supabase } from "../supabase/supabase.js";
 
 export const userRouter = Router();
 
@@ -8,6 +9,23 @@ userRouter.get('/', (req, res) => {
     res.json({message: 'Hello World'});
 });
 
-userRouter.post('/', userValidation, (req, res) => {
-    res.json(req.body);
+userRouter.post('/', userValidation, async (req, res) => {
+    const { data, error } = await supabase
+        .from('Users')
+        .insert({
+            email: req.body.email,
+            password: req.body.password,
+            firstname: req.body.firstName,
+            lastname: req.body.lastName,
+            role: req.body.role,
+            country: req.body.location.country,
+            state: req.body.location.state,
+            city: req.body.location.city
+    });
+
+    console.log(data, error);
+    if(error) {
+        return res.status(409).json({error: error.message});
+    }
+    return res.status(201).json({message: "User created successfully"});
 });
